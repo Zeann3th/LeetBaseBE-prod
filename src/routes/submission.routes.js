df9b2c40224c3e6cd4044a5bce2a94e6be5b941f
zeann3th <longhienly112@gmail.com>
2025-03-08 19:48:45 +0700

    fix: printing req.body of callback

--- src/routes/submission.routes.js
+++ src/routes/submission.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import SubmissionController from "../controllers/submission.controller.js";
+import { verifyUser } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/:id", SubmissionController.getById);
+router.get("/:id", verifyUser, SubmissionController.getById);
 
-router.post("/", SubmissionController.create);
+router.post("/", verifyUser, SubmissionController.create);
 
 router.put("/callback", SubmissionController.createCallback);
 

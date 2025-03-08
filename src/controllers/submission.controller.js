d27fbe4d2163f3789b8540e0d2fa99e19d0bd454
zeann3th <longhienly112@gmail.com>
2025-03-08 18:10:54 +0700

    fix: printing req.body of callback

--- src/controllers/submission.controller.js
+++ src/controllers/submission.controller.js
@@ -16,17 +16,18 @@ const languages = new TwoWayMap({
 
 const getById = async (req, res) => {
   const id = sanitize(req.params.id, "uuid");
+  console.log(id, typeof id);
   if (!id) {
     return res.status(400).json({ message: "Invalid Submission Id" });
   }
   try {
-    const submission = await Submission.findById(id);
+    const submission = await Submission.find({ submissionId: { $eq: id } });
     if (!submission) {
       return res.status(404).json({ message: "Submission not found" });
     }
-    if (submission.user.toString() !== req.user.sub) {
-      return res.status(403).json({ message: "Unauthorized" });
-    }
+    //if (submission.user.toString() !== req.user.sub) {
+    //  return res.status(403).json({ message: "Unauthorized" });
+    //}
     if (submission.status === "PENDING") {
       return res.status(202).json({ message: "Submission is pending" });
     }
@@ -50,46 +51,40 @@ const create = async (req, res) => {
     return res.status(400).json({ message: "Invalid programming language" })
   }
 
-  const problem = await Problem.findById(problemId);
-  if (!problem) {
-    return res.status(404).json({ message: "Problem not found" });
-  }
-
-  const template = await s3.getDownloadUrl(`${problemId}/${language.toLowerCase()}`);
-  if (!template) {
-    return res.status(404).json({ message: "Template not found" });
-  }
-
-  const submit = template.replace(/\/\/--code--/g, atob(code));
-
-  const options = {
-    method: 'POST',
-    url: 'https://judge0-ce.p.rapidapi.com/submissions',
-    params: {
-      base64_encoded: 'true',
-      wait: 'false',
-      fields: '*'
-    },
-    headers: {
-      "x-rapidapi-key": process.env.RAPID_API_KEY,
-      "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-      "Content-Type": "application/json"
-    },
-    data: {
-      language_id: languageId,
-      source_code: btoa(submit),
-      callback_url: `${process.env.BASE_URL}/v1/submissions/callback`
-    }
-  };
-
   try {
+    const problem = await Problem.findById(problemId);
+    if (!problem) {
+      return res.status(404).json({ message: "Problem not found" });
+    }
+
+    const template = await s3.getDownloadUrl(`${problemId}/${language.toLowerCase()}`);
+    if (!template) {
+      return res.status(404).json({ message: "Template not found" });
+    }
+
+    const submit = template.replace(/\/\/--code--/g, atob(code));
+
+    const options = {
+      method: 'POST',
+      url: 'https://judge0-ce.p.rapidapi.com/submissions',
+      params: {
+        base64_encoded: 'true',
+        wait: 'false',
+        fields: '*'
+      },
+      headers: {
+        "x-rapidapi-key": process.env.RAPID_API_KEY,
+        "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+        "Content-Type": "application/json"
+      },
+      data: {
+        language_id: languageId,
+        source_code: btoa(submit),
+        callback_url: `${process.env.BASE_URL}/v1/submissions/callback`
+      }
+    };
+
     const { data: { token: submissionId } } = await axios.request(options);
-    const submission = await Submission.create({
-      submissionId,
-      user: req.user.sub,
-      problem: problemId,
-      language,
-    })
     res.status(202).json({ submissionId });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -97,6 +92,7 @@ const create = async (req, res) => {
 }
 
 const createCallback = async (req, res) => {
+  console.log(req.body);
   return res.status(200).json(req.body);
 }
 
